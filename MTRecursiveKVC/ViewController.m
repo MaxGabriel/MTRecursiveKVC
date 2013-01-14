b5ec20a67037d8ae6c434a32ed22d91b28368cc4
Maximilian Tagher <feedback.tagher@gmail.com>
2013-01-13 22:00:44 -0800

    Removed unused OCUnit tests. Removed label code from view controller (from SO example)

--- MTRecursiveKVC/ViewController.m
+++ MTRecursiveKVC/ViewController.m
@@ -18,28 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 100, 300, 50)];
-    label.text = @"Varying text length here. It gets really really really long and wraps to the next line";
-    label.numberOfLines = 0;
-    [label sizeToFit];
-    [self.view addSubview:label];
-    NSLog(@"Label's frame = %@",NSStringFromCGRect(label.frame));
-    
-    UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectOffset(label.frame, 0, CGRectGetHeight(label.frame)+0)];
-    label2.text = @"This one is unpredictably shorter!";
-    label2.numberOfLines = 0;
-    [label2 sizeToFit];
-    [self.view addSubview:label2];
-    
-    
-    
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
