154e1161ef7d7ebe86c6c55a219e9e3915ca517a
Maximilian Tagher <feedback.tagher@gmail.com>
2013-01-14 19:35:58 -0800

    Added preliminary documentation for the NSObject version of `recursiveValueForKey:`

--- MTRecursiveKVC/NSObject+RecursiveKVC.h
+++ MTRecursiveKVC/NSObject+RecursiveKVC.h
@@ -10,6 +10,23 @@
 
 @interface NSObject (RecursiveKVC)
 
+/** Returns the result of recursively calling `valueForKey:` on each returned object until it reaches a `nil` value.
+ 
+ Applies the standard `valueForKey:` method recursively along a chain of objects.
+ Recursively applies `valueForKey:` to the receiver.
+ Returns a recursive
+ Returns the result of `valueForKey:`
+ 
+ 'and then recursively applies the method until it reaches a `nil` value'
+ 
+ @param key The name of one of the receiver's properties.
+ @return The objects returned by recursively calling the `valueForKey:` method on objects returned by `valueForKey:`
+ 
+ The result of recursively calling `valueForKey:`
+
+ 
+ */
+
 - (NSArray *)recursiveValueForKey:(NSString *)key;
 
 @end
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios
+  target :MTRecursiveKVCTests, :exclusive => true do
+      pod 'Kiwi'
+  end
